9f69614336be6461c5674f3e56de42573dd0c219
iyataryouway <iyanahcamille.taryouway@rdpolytech.ca>
2024-12-05 13:49:57 -0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  //Initialize Flutter bindings
+  WidgetsFlutterBinding().ensureInitialized();
+
+  //Initialize FIrebase with the current platform's default options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
