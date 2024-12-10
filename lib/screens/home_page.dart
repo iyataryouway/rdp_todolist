3acab4aa9b899e93187f3d36d54b10df32af623c
iyataryouway <iyanahcamille.taryouway@rdpolytech.ca>
2024-12-10 13:14:02 -0700

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:rdp_todolist/services/firestore_services.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,41 +15,47 @@ class _HomePageState extends State<HomePage> {
       FirebaseFirestore.instance; //new firestore instance
   final TextEditingController nameController =
       TextEditingController(); //captures textform input
+  final List<Map<String, dynamic>> tasks = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                  child: Image.asset(
-                'assets/rdplogo.png',
-                height: 80,
-              )),
-              const Text(
-                'Daily Planner',
-                style: TextStyle(
-                    fontFamily: 'Caveat', fontSize: 32, color: Colors.white),
-              )
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Image.asset('assets/rdplogo.png', height: 80),
+            ),
+            const Text(
+              'Daily Planner',
+              style: TextStyle(
+                  fontFamily: 'Caveat', fontSize: 32, color: Colors.white),
+            )
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              TableCalendar(
-                calendarFormat: CalendarFormat.month,
-                focusedDay: DateTime.now(),
-                firstDay: DateTime(2024),
-                lastDay: DateTime(2025),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TableCalendar(
+                    calendarFormat: CalendarFormat.month,
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2024),
+                    lastDay: DateTime(2025),
+                  ),
+                ],
               ),
-              buildAddTaskSection(nameController),
-            ],
+            ),
           ),
-        ));
+          buildAddTaskSection(nameController),
+        ],
+      ),
+    );
   }
 }
 
@@ -60,6 +65,7 @@ Widget buildAddTaskSection(nameController) {
     controller: nameController,
     decoration: const InputDecoration(
       labelText: 'Add Task',
+      border: OutlineInputBorder(),
     ),
   );
 }
